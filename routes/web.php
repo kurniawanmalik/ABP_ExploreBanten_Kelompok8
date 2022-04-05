20f0cb6f00be7f474f41e87ad2ca685bc59bb3d7
Guido Tamara <102816841+Grendozramy@users.noreply.github.com>
2022-04-06 01:16:53 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/alam', function () {
+    return view('alam');
+});
+
+Route::get('/budaya', function () {
+    return view('budaya');
+});
+Route::get('/kuliner', function () {
+    return view('kuliner');
+});
+
+Route::get('/news', function () {
+    return view('news');
+});
+
+Route::get('/maps', function () {
+    return view('maps');
+});
+
+Route::get('/reviews', function () {
+    return view('reviews');
+});
